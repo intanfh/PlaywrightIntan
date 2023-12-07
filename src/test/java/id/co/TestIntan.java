04e561c073e2988618330bcda16baee0ec9870d0
Intan heryan <intanfh00@gmail.com>
2023-12-07 08:53:22 +0700

    Login Positive Test

---
+++ src/test/java/id/co/TestIntan.java
@@ -0,0 +1,31 @@
+package id.co;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestIntan {
+    @Test
+    @DisplayName("Test Login Positve Test")
+    public void LoginPositiveTest() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+
+        page.navigate("https://practicetestautomation.com/practice-test-login/");
+        page.locator("#username").type("student");
+        page.locator("#password").type("Password123");
+        page.locator("#submit").click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions()
+                .setName("submit")).last().click();
+
+        page.close();
+        browser.close();
+        playwright.close();
+
+    }
+}
